e5fed38173f9a0d7b7b693dfc34d3464dec37f4d
brittonr <b@robitzs.ch>
2026-04-01 18:11:02 -0400

    fix: update flake to include ratcore as source input
    Adds github:brittonr/ratcore as a non-flake input. The nix build
    copies ratcore into the source tree and rewrites the path dep so
    unit2nix can find it in the sandbox.

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     unit2nix.url = "github:brittonr/unit2nix";
     flake-utils.url = "github:numtide/flake-utils";
+    ratcore = {
+      url = "github:brittonr/ratcore";
+      flake = false;
+    };
   };
 
   outputs =
@@ -12,6 +16,7 @@
       nixpkgs,
       unit2nix,
       flake-utils,
+      ratcore,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -19,9 +24,16 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
+        # Combine subwayrat source with ratcore (path dep at ../ratcore)
+        src = pkgs.runCommand "subwayrat-src" {} ''
+          cp -r ${./.} $out
+          chmod -R u+w $out
+          cp -r ${ratcore} $out/ratcore
+          sed -i 's|path = "../ratcore"|path = "ratcore"|' $out/Cargo.toml
+        '';
+
         ws = unit2nix.lib.${system}.buildFromUnitGraphAuto {
-          inherit pkgs;
-          src = ./.;
+          inherit pkgs src;
         };
       in
       {
